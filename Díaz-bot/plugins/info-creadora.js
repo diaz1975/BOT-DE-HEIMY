a28ea316025e15bd07a77ebc8118708383482e57
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-12-29 14:47:25 -0500

    Update info-creadora.js

--- Díaz-bot/plugins/info-creadora.js
+++ Díaz-bot/plugins/info-creadora.js
@@ -2,7 +2,7 @@ var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = gataImg.getRandom()	
 const cat = `
-〔 𝗗𝗶𝗮𝘇 𝗕𝗼𝘁🥷🏻 〕
+〔 𝘽𝙤𝙩 𝘿𝙚 𝙃𝙚𝙞𝙢𝙮 👑 〕
 
 *Desarrollador : +57 323 423 1940*
 *Nombre : Diaz Mod*
